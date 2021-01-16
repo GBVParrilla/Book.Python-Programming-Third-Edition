d242b7d89901584c8492b2c59a3b4e92aaec99aa
GBVParrilla <74801989+GBVParrilla@users.noreply.github.com>
2021-01-16 14:18:13 +0800

    Add files via upload
    Chapter 1 of the book

---
+++ Chapter 1.py
@@ -0,0 +1,8 @@
+#Getting Started In Python
+#Why python is good
+#Python is easy to use, powerful, object-oriented, is a glue language, runs everywhere, has a strong community, is freer and open source.
+print("Game Over")
+#print() is a function, functions are mini programs that do a specific tasks.
+#Any symbols in the "" are called strings.
+#Make comments using the hashtag sign
+#print() prints the functions/strings inside the ()
